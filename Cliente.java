303f7224edb6138824c40c04607e64e72cab4d89
AnaGiori <162601303+AnaGiori@users.noreply.github.com>
2025-01-13 14:05:26 -0300

    Update Cliente.java 2.0

--- Cliente.java
+++ Cliente.java
@@ -27,12 +27,24 @@ class Cliente {
         return this.cpf.equals(cpf) && this.senha.equals(senha);
     }
 
-    public void criarConta(String tipo) {
-    int numeroConta = Conta.totalContas++;
-    Conta novaConta = new Conta(numeroConta, tipo, 0.0);
-    contas.add(novaConta);
-    System.out.println("Conta criada! Número: " + numeroConta);
+   public void criarConta(Agencia agencia, String tipo) {
+        String numeroConta = agencia.gerarNumeroConta();
+        Conta novaConta;
+        switch (tipo) {
+            case "ContaCorrente":
+                novaConta = new ContaCorrente(agencia, numeroConta);
+                break;
+            case "ContaPoupanca":
+                novaConta = new ContaPoupanca(agencia, numeroConta);
+                break;
+            case "ContaSalario":
+                novaConta = new ContaSalario(agencia, numeroConta);
+                break;
+            default:
+                System.out.println("Tipo de conta não reconhecido.");
+                return;
+        }
+        contas.add(novaConta);
+        System.out.println("Conta criada! Número: " + numeroConta);
     }
-
 }
-
